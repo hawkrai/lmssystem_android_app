5308145b8d556b5299549ce464a2b74796c98713
Fox <codezfox@live.ru>
2016-04-14 16:57:42 +0300

    init project

---
+++ app/src/main/java/com/celt/lms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.celt.lms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
