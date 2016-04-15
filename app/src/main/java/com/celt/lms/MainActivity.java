48c79ed6aabcba4bc2b0952ca9bd12c58c64b142
Fox <codezfox@live.ru>
2016-04-15 16:16:36 +0300

    added toolbar

--- app/src/main/java/com/celt/lms/MainActivity.java
+++ app/src/main/java/com/celt/lms/MainActivity.java
@@ -6,9 +6,18 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
